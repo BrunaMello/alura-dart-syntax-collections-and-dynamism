8e8a590d427c573a559624dbbe90d2bbd40305f0
brunamello <brunacrespomello@gmail.com>
2022-11-26 16:44:26 +0000

    Initial commit

---
+++ bin/alura_dart_syntax_collections_and_dynamism.dart
@@ -0,0 +1,5 @@
+import 'package:alura_dart_syntax_collections_and_dynamism/alura_dart_syntax_collections_and_dynamism.dart' as alura_dart_syntax_collections_and_dynamism;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alura_dart_syntax_collections_and_dynamism.calculate()}!');
+}
---
+++ test/alura_dart_syntax_collections_and_dynamism_test.dart
@@ -0,0 +1,8 @@
+import 'package:alura_dart_syntax_collections_and_dynamism/alura_dart_syntax_collections_and_dynamism.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
