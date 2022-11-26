1d8765e4a7cde4f68b8b3f324fb0d688c9bb984e
brunamello <brunacrespomello@gmail.com>
2022-11-26 21:00:20 +0000

    2 - Collections

--- bin/alura_dart_syntax_collections_and_dynamism.dart
+++ bin/alura_dart_syntax_collections_and_dynamism.dart
@@ -1,7 +1,35 @@
 void main() {
 
-  chooseTransportTypeEnum(TransportType.skate);
-  chooseTransportTypeSwitch(TransportType.car);
+
+  // // Sets (uniques values)
+  // Set<String> visitedDestinies = <String>{};
+  // visitedDestinies = registryDestiny("Rio de Janeiro", visitedDestinies);
+  // visitedDestinies = registryDestiny("Sao Paulo", visitedDestinies);
+  // visitedDestinies = registryDestiny("Salvador", visitedDestinies);
+  // print(visitedDestinies);
+
+  //Iterables
+  // print(visitedDestinies.first);
+  // print(visitedDestinies.last);
+  // print(visitedDestinies.isEmpty);
+  // print(visitedDestinies.length);
+  //
+  // print(visitedDestinies.elementAt(2));
+  // print(visitedDestinies.contains("Sao Paulo"));
+  // print(visitedDestinies.contains("Minas Gerais"));
+
+  // Maps (objeto em relacao ao outro,  em outras linguagem chamada de dicionario)
+
+  Map<String, dynamic> registryPrices = {};
+  registryPrices["Sao Paulo"] = 1200;
+  registryPrices["Rio de Janeiro"] = 1500;
+  registryPrices["Salvador"] = 1250;
+  registryPrices["New York"] = "too expensive";
+
+  registryPrices.remove("Sao Paulo");
+
+  print(registryPrices);
+
   }
 
 //Enums
@@ -16,29 +44,29 @@ enum TransportType{
   train,
 }
 
-void chooseTransportType(int transport) {
-  if (transport == 0){
-    print("Go by car for the adventure");
-  } else if (transport == 1){
-    print("Go by bike for the Adventure");
-  } else {
-    print("Go for the Adventure");
-  }
-}
+// void chooseTransportType(int transport) {
+//   if (transport == 0){
+//     print("Go by car for the adventure");
+//   } else if (transport == 1){
+//     print("Go by bike for the Adventure");
+//   } else {
+//     print("Go for the Adventure");
+//   }
+// }
 
-void chooseTransportTypeEnum(TransportType transport){
-  if (transport == TransportType.car){
-    print("Go by car for the Adventure");
-  } else if (transport == TransportType.bike){
-    print("Go by bike for the Adventure");
-  } else {
-    print("Go for the Adventure");
-  }
-}
+// void chooseTransportTypeEnum(TransportType transport){
+//   if (transport == TransportType.car){
+//     print("Go by car for the Adventure");
+//   } else if (transport == TransportType.bike){
+//     print("Go by bike for the Adventure");
+//   } else {
+//     print("Go for the Adventure");
+//   }
+// }
 
 //Switch Case
 
-void chooseTransportTypeSwitch(TransportType transport){
+void chooseTransportType(TransportType transport){
 
   switch (transport) {
     case TransportType.bike:
@@ -54,4 +82,25 @@ void chooseTransportTypeSwitch(TransportType transport){
       print("Go to the Adventure");
   }
 }
+
+// Sets
+
+Set<String> registryDestiny(String destiny, Set<String> database){
+  database.add(destiny);
+  return database;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
