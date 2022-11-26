8af6bfd99c74ed6508bbae081de7bf28e5ced8ad
brunamello <brunacrespomello@gmail.com>
2022-11-26 17:17:25 +0000

    1 - Syntax

--- bin/alura_dart_syntax_collections_and_dynamism.dart
+++ bin/alura_dart_syntax_collections_and_dynamism.dart
@@ -1,5 +1,57 @@
-import 'package:alura_dart_syntax_collections_and_dynamism/alura_dart_syntax_collections_and_dynamism.dart' as alura_dart_syntax_collections_and_dynamism;
+void main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${alura_dart_syntax_collections_and_dynamism.calculate()}!');
+  chooseTransportTypeEnum(TransportType.skate);
+  chooseTransportTypeSwitch(TransportType.car);
+  }
+
+//Enums
+
+enum TransportType{
+  car,
+  bike,
+  foot,
+  skate,
+  plane,
+  roller,
+  train,
 }
+
+void chooseTransportType(int transport) {
+  if (transport == 0){
+    print("Go by car for the adventure");
+  } else if (transport == 1){
+    print("Go by bike for the Adventure");
+  } else {
+    print("Go for the Adventure");
+  }
+}
+
+void chooseTransportTypeEnum(TransportType transport){
+  if (transport == TransportType.car){
+    print("Go by car for the Adventure");
+  } else if (transport == TransportType.bike){
+    print("Go by bike for the Adventure");
+  } else {
+    print("Go for the Adventure");
+  }
+}
+
+//Switch Case
+
+void chooseTransportTypeSwitch(TransportType transport){
+
+  switch (transport) {
+    case TransportType.bike:
+      print("Go by bike");
+      break;
+    case TransportType.car:
+      print("Go by car for the Adventure");
+      break;
+    case TransportType.foot:
+      print("Go by foot for the Adventure");
+      break;
+    default:
+      print("Go to the Adventure");
+  }
+}
+
